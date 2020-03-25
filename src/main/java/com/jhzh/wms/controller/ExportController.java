9e03c4674f7970e6910694ecff1177cd6a26364b
1028408511@qq.com <Jansen102840>
2020-03-25 15:00:58 +0800

    10  3-25提交代码

--- src/main/java/com/jhzh/wms/controller/ExportController.java
+++ src/main/java/com/jhzh/wms/controller/ExportController.java
@@ -35,9 +35,22 @@ public class ExportController {
         @Cleanup
         OutputStream outputStream=null;
         System.out.println(exportConfig);
+        Map<String,String>titleMap=new HashMap<>();
+        titleMap.put("taskid","任务号");
+        titleMap.put("tasksource","任务来源标识");
+        titleMap.put("locator","入口位编码");
+        titleMap.put("shelfcode","多层载具载具号");
+        titleMap.put("shelflay","层次号");
+        titleMap.put("wipentityid","工单号");
+        titleMap.put("stockno","托盘编号");
+        titleMap.put("lotcode","物料批次号");
+        titleMap.put("ismultipalforlot","允许分拆批次号");
+        titleMap.put("quantity","数量");
+        titleMap.put("itemcode","物料号");
+        titleMap.put("partdate","存入日期");
+        titleMap.put("status","状态");
         List<Map<String, Object>> listMap = putInStorageDao.queryWmsInvInFlow(new HashMap());
-        Map<String,String> map =new HashMap<>();
-        HSSFWorkbook hssfWorkbook = ExportUtil.exportExcel(listMap,exportConfig.getWmsInvInFlow());
+        HSSFWorkbook hssfWorkbook = ExportUtil.exportExcel(listMap,titleMap);
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletResponse response = requestAttributes.getResponse();
         response.setContentType("application/xls;charset=UTF-8");
